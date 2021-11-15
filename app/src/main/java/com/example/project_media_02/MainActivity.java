f2769d5863400c612dd6e36e5447268e958fee81
AiswariaNRaj <aiswarianraj1996@gmail.com>
2021-11-15 11:29:28 +0530

    Initial Commit

--- app/src/main/java/com/example/project_media_02/MainActivity.java
+++ app/src/main/java/com/example/project_media_02/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.project_media_02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_main_tl_tablayout);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_vp_viewpager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Now Playing"));
+        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+
+        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
 }
