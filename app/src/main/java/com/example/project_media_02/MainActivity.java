12c8cfcce4b0aef0844e97c3dc54b3a3b03d6f42
AiswariaNRaj <aiswarianraj1996@gmail.com>
2021-11-20 22:13:48 +0530

    Initial Commit

--- app/src/main/java/com/example/project_media_02/MainActivity.java
+++ app/src/main/java/com/example/project_media_02/MainActivity.java
@@ -1,47 +1,213 @@
 package com.example.project_media_02;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
+import java.util.List;
+
+import SepratePackage.aidlInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    private aidlInterface aidlObject;
+    int PreviousResult,CurrentResult,NextResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_main_tl_tablayout);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_vp_viewpager);
+//        bindToAIDLService();
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_main_tl_tablayout);
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_vp_viewpager);
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Now Playing"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+//        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+//
+//        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),tabLayout.getTabCount());
+//        viewPager.setAdapter(adapter);
+//
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//
+//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+//
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+//
+//
+//
+//        try {
+//            aidlObject.playmusic();
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
 
-        tabLayout.addTab(tabLayout.newTab().setText("Now Playing"));
-        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
 
-        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(),tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
 
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        TextView textView_SongName = findViewById(R.id.activity_main_tv_songname);
+        TextView textView_Album = findViewById(R.id.activity_main_tv_album);
+        TextView textView_Artist = findViewById(R.id.activity_main_tv_artist);
+        ImageButton imageButton_Previous = findViewById(R.id.activity_main_ib_previous);
+        ImageButton imageButton_PlayPause = findViewById(R.id.activity_main_ib_playpause);
+        ImageButton imageButton_Next = findViewById(R.id.activity_main_ib_next);
 
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+
+//        View.OnClickListener onClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//
+//            switch (view.getId()) {
+//
+//                case R.id.activity_main_ib_previous:
+//                     //PlayPreviousSong();
+//                    try {
+//                        PreviousResult = aidlObject.PerformPrevious();
+//                    } catch (RemoteException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+//
+//                case R.id.activity_main_ib_playpause:
+//                     //PlayCurrentSong();
+//                    try {
+//                        CurrentResult = aidlObject.PerformCurrent();
+//                    } catch (RemoteException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+//
+//                case R.id.activity_main_ib_next:
+//                   //  PlayNextSong();
+//                    try {
+//                        NextResult = aidlObject.PerformNext();
+//                    } catch (RemoteException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+//
+//                default:
+//                     break;
+//
+//
+//            }
+//        }
+
+        imageButton_Previous.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
+            public void onClick(View v) {
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+                try {
+                    PreviousResult = aidlObject.PerformPreviousPlay();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+
+        imageButton_PlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // PlayCurrentSong();
+                try {
+                    CurrentResult = aidlObject.PerformCurrentPlay();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        imageButton_Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // PlayNextSong();
+                try {
+                    NextResult = aidlObject.PerformNextPlay();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        bindToAIDLService();
+
+}
+
+//    private int PlayCurrentSong() {
+//        return 0;
+//    }
+//
+//    private int PlayNextSong() {
+//        return 0;
+//    }
+//
+//
+//    private int PlayPreviousSong() {
+//        return 0;
+//    }
+
+    private void bindToAIDLService() {
+        Intent aidlServiceIntent = new Intent("connect_to_aidl_service");
+        bindService(convertImplicitIntentToExplicitIntent(aidlServiceIntent,this), serviceConnectionObject, BIND_AUTO_CREATE);
     }
 
+    ServiceConnection serviceConnectionObject = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            aidlObject = aidlInterface.Stub.asInterface(service);
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+
+    public Intent convertImplicitIntentToExplicitIntent(Intent implicitIntent, Context context) {
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> resolveInfoList = pm.queryIntentServices(implicitIntent, 0);
+
+        if (resolveInfoList == null || resolveInfoList.size() != 1) {
+            return null;
+        }
+        ResolveInfo serviceInfo = resolveInfoList.get(0);
+        ComponentName component = new ComponentName(serviceInfo.serviceInfo.packageName, serviceInfo.serviceInfo.name);
+        Intent explicitIntent = new Intent(implicitIntent);
+        explicitIntent.setComponent(component);
+        return explicitIntent;
+
+    }
 }
--- app/src/main/java/com/example/project_media_02/NowPlaying_Fragment.java
+++ app/src/main/java/com/example/project_media_02/NowPlaying_Fragment.java
@@ -1,14 +1,21 @@
 package com.example.project_media_02;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +23,17 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class NowPlaying_Fragment extends Fragment {
+    private final MediaControllerCompat.Callback mediaControllerCallback = new MediaControllerCompat.Callback() {
+        @Override
+        public void onPlaybackStateChanged(PlaybackStateCompat state) {
+            super.onPlaybackStateChanged(state);
+        }
+
+        @Override
+        public void onMetadataChanged(MediaMetadataCompat metadata) {
+            super.onMetadataChanged(metadata);
+        }
+    };
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,51 +64,143 @@ public class NowPlaying_Fragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
+
+
+    //    private View.OnClickListener onClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            //MediaControllerCompat.TransportControls controls = MediaControllerCompat.getMediaController(getActivity()).getTransportControls();
+//
+//
+//            switch (view.getId()) {
+//                case R.id.fr_now_playing_ib_playpause:
+//                    play();
+////                    PlaybackStateCompat state = MediaControllerCompat.getMediaController(NowPlaying_Fragment.this.getActivity()).getPlaybackState();
+////
+////                    if (state != null) {
+////                        switch (state.getState()) {
+////                            case PlaybackStateCompat.STATE_PLAYING:
+////                            case PlaybackStateCompat.STATE_BUFFERING:
+////                                controls.pause();
+////                                break;
+////                            case PlaybackStateCompat.STATE_PAUSED:
+////                            case PlaybackStateCompat.STATE_STOPPED:
+////                                controls.play();
+////                                break;
+////
+////                        }
+////                    }
+//                    Toast.makeText(getActivity(), "Play Music", Toast.LENGTH_LONG).show();
+//                    break;
+//
+//                case R.id.fr_now_playing_ib_next:
+//                    //controls.skipToNext();
+//                    Toast.makeText(getActivity(), "Play Next Music", Toast.LENGTH_LONG).show();
+//                    break;
+//
+//                case R.id.fr_now_playing_ib_previous:
+//                    //controls.skipToPrevious();
+//                    Toast.makeText(getActivity(), "Play Previous Music", Toast.LENGTH_LONG).show();
+//                    break;
+//
+//                default:
+//                    break;
+//
+//            }
+//        }
+//
+//        private void play() {
+//        }
+//
+//    };
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_now_playing_, container, false);
 
-        TextView textView_SongName = (TextView) v.findViewById(R.id.fr_now_playing_tv_songname);
-        TextView textView_Album = (TextView) v.findViewById(R.id.fr_now_playing_tv_album);
-        TextView textView_Artist = (TextView) v.findViewById(R.id.fr_now_playing_tv_artist);
-        ImageButton imageButton_Previous = (ImageButton) v.findViewById(R.id.fr_now_playing_ib_previous);
-        ImageButton imageButton_PlayPause = (ImageButton) v.findViewById(R.id.fr_now_playing_ib_playpause);
-        ImageButton imageButton_Next = (ImageButton) v.findViewById(R.id.fr_now_playing_ib_next);
+//        TextView textView_SongName = (TextView) v.findViewById(R.id.fr_now_playing_tv_songname);
+//        TextView textView_Album = (TextView) v.findViewById(R.id.fr_now_playing_tv_album);
+//        TextView textView_Artist = (TextView) v.findViewById(R.id.fr_now_playing_tv_artist);
+//        ImageButton imageButton_Previous = (ImageButton) v.findViewById(R.id.fr_now_playing_ib_previous);
+//        ImageButton imageButton_PlayPause = (ImageButton) v.findViewById(R.id.fr_now_playing_ib_playpause);
+//        ImageButton imageButton_Next = (ImageButton) v.findViewById(R.id.fr_now_playing_ib_next);
+//        Drawable PlayDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ic_baseline_play_arrow_24);
+//        Drawable PauseDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ic_baseline_pause_24);
+//
+//
+//        imageButton_PlayPause.setOnClickListener(onClickListener);
+//        imageButton_Next.setOnClickListener(onClickListener);
+//        imageButton_Previous.setOnClickListener(onClickListener);
 
-        imageButton_Previous.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
+       return v;
 
-        imageButton_PlayPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        imageButton_Next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-      return v;
     }
+
+
+
+
+//    private View.OnClickListener onClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//
+//            switch (view.getId()) {
+//                case R.id.fr_now_playing_ib_playpause:
+//
+//                    PlaySong();
+//
+//                     break;
+//
+//                case R.id.fr_now_playing_ib_next:
+//                     NextSong();
+//                     Toast.makeText(getActivity(), "Play Next Music", Toast.LENGTH_LONG).show();
+//                     break;
+//
+//                case R.id.fr_now_playing_ib_previous:
+//                     PreviousSong();
+//                     Toast.makeText(getActivity(), "Play Previous Music", Toast.LENGTH_LONG).show();
+//                     break;
+//
+//                default:
+//                     break;
+//
+//
+//            }
+//        }
+//
+//
+//
+//    };
+//
+//    private void PlaySong() {
+//        Log.d("Play:","playsong");
+//
+//    }
+//
+//    private void NextSong() {
+//
+//    }
+//
+//    private void PreviousSong() {
+//
+//    }
+
 }
+
+
