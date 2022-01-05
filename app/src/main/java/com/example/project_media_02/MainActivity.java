cadf24a8dea8600ce641a93f5b455b4b9d962006
akhil <akhileshsb123@gmail.com>
2022-01-05 16:28:37 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/project_media_02/ViewPagerAdapter.java

--- app/src/main/java/com/example/project_media_02/MainActivity.java
+++ app/src/main/java/com/example/project_media_02/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Cache
+
         if (getSupportFragmentManager().findFragmentById(android.R.id.content)==null) {
             getSupportFragmentManager().beginTransaction()
                     .add(android.R.id.content, new MainFragment())
